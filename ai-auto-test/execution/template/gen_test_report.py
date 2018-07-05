1e4f5f94460fcbbec08a3762344dbc1d1c137ead
Zhan Lingling <zhanlingling_00@163.com>
2018-07-05 17:25:23 +0800

    Update excel test report.

--- ai-auto-test/execution/template/gen_test_report.py
+++ ai-auto-test/execution/template/gen_test_report.py
@@ -45,7 +45,7 @@ def gen_report(test_plan, results_dir, report_fn):
     result_header_fmt = wb.add_format(header_fmt)
     result_fmt = wb.add_format(fmt)
 
-    # # Pass/Fail results formats.
+    # Results formats.
     # pass_fmt = wb.add_format(
     #     {'bg_color': '#C6EFCE', 'font_color': '#006100', 'border': 1})
     # fail_fmt = wb.add_format(
@@ -53,9 +53,10 @@ def gen_report(test_plan, results_dir, report_fn):
     # na_fmt = wb.add_format(
     #     {'bg_color': '#FFEB9C', 'font_color': '#9C8000', 'border': 1})
 
-    # Create table in summary sheet
+    # Create table in summary sheet.
     summary_ws = wb.add_worksheet('Summary')
 
+    # Create heading of summary table.
     headings = ['Pass', 'Fail', 'N/A', 'Total', 'Pass Rate']
     summary_ws.set_column('%s:%s' % (chr(65), chr(len(headings) + 65)), 20)
     for i in range(len(headings)):
@@ -69,70 +70,102 @@ def gen_report(test_plan, results_dir, report_fn):
     ws = wb.add_worksheet(sheet_name)
 
     # Create table header.
-    headings = ['Case Name', 'Test Result', 'Comments']
+    headings = [
+        'Case Name',
+        'Pre-Condition',
+        'Steps',
+        'Expected Results',
+        'Test Result',
+        'Comments']
     for i in range(len(headings)):
         ws.write_string(0, i, headings[i], result_header_fmt)
-    ws.set_column('A:A', 60)
-    ws.set_column('B:B', 15)
-    ws.set_column('C:C', 50)
+        ws.set_column('%s:%s' % (chr(i + 65), chr(i + 65)), 45)
+    res_col = chr(headings.index('Test Result') + 65)
+    ws.set_column('%s:%s' % (res_col, res_col), 10)
+    ws.set_zoom(85)
 
+    data_lst = [list() for i in range(len(headings))]
     # row = 1
-    data_lst = [case_lst]
-    res_lst = list()
-    comments_lst = list()
     for test_case in case_lst:
+        # tc_info = []
+
+        # Parse test case information.
+        case_script = os.path.join(os.pardir, os.pardir, 'cases', test_case)
+        content = open(case_script, 'r').read()
+        s = re.search('Case Description(.*)End Description', content, re.S)
+        if s:
+            tc_des = s.groups()[0].strip()
+        s = re.search(
+            'Pre-Condition:(.*)Case Name:(.*)Test Steps:(.*)Expected Results:(.*)',
+            tc_des,
+            re.S)
+        if s:
+            for i in range(len(s.groups())):
+                c = [unicode(line.strip(), 'utf-8')
+                     for line in s.groups()[i].split('\n')]
+                data_lst[i].append('\n'.join(c).strip())
+                # tc_info.append('\n'.join(c).strip())
+        else:
+            for i in range(4):
+                data_lst[i].append('')
+                # tc_info.append('')
+
+        # Parse test results.
         case_log = os.path.join(results_dir, test_case, test_case + '.log')
         comments = ''
         if os.path.exists(case_log):
             # Get test result.
-            f = re.compile('Test Result: (.*)').findall(open(case_log).read())
+            f = re.findall('Test Result: (.*)', open(case_log).read())
             if f:
                 test_res = f[-1]
             else:
-                test_res = 'FAIL'
+                test_res = 'fail'
 
             # Get comments.
-            f = re.compile('Comments: (.*)').findall(open(case_log).read())
+            f = re.findall('Comments: (.*)', open(case_log).read())
             if f:
                 comments = f[-1]
             else:
-                if test_res == 'FAIL':
+                if test_res == 'fail':
                     comments = 'Fail to run test case.'
         else:
-            test_res = 'FAIL'
+            test_res = 'fail'
             comments = 'Fail to run test case.'
+
         test_res = test_res.strip().lower()
-        res_lst.append(test_res)
-        comments_lst.append(comments)
+        data_lst[4].append(test_res)
+        data_lst[5].append(comments)
+        # tc_info.append(test_res)
+        # tc_info.append(comments)
         # if test_res == 'pass':
         #     tmp_fmt = pass_fmt
         # elif test_res == 'fail':
         #     tmp_fmt = fail_fmt
         # else:
         #     tmp_fmt = na_fmt
-        # ws.write_string(row, 0, test_case, tmp_fmt)
-        # ws.write_string(row, 1, test_res, tmp_fmt)
-        # ws.write_string(row, 2, comments, tmp_fmt)
+        # for i in range(len(tc_info)):
+        #     ws.write_string(row, i, tc_info[i], tmp_fmt)
         # row += 1
-    data_lst.append(res_lst)
-    data_lst.append(comments_lst)
-    ws.write_column(1, 0, data_lst[0], result_fmt)
-    ws.write_column(1, 1, data_lst[1], result_fmt)
-    ws.write_column(1, 2, data_lst[2], result_fmt)
 
-    summary_ws.write_formula(1, 0, "=_xlfn.COUNTIF('%s'!B2:B%d, \"pass\")" %
-                             (sheet_name, len(case_lst) + 1), summary_num_fmt)
-    summary_ws.write_formula(1, 1, "=_xlfn.COUNTIF('%s'!B2:B%d, \"fail\")" %
-                             (sheet_name, len(case_lst) + 1), summary_num_fmt)
+    # Write test case information and results to result sheet.
+    for i in range(len(data_lst)):
+        ws.write_column(1, i, data_lst[i], result_fmt)
+
+    # Write summary table of test results.
+    summary_ws.write_formula(1, 0, "=_xlfn.COUNTIF('%s'!%s:%s, \"pass\")" %
+                             (sheet_name, res_col, res_col), summary_num_fmt)
+    summary_ws.write_formula(1, 1, "=_xlfn.COUNTIF('%s'!%s:%s, \"fail\")" %
+                             (sheet_name, res_col, res_col), summary_num_fmt)
     summary_ws.write_formula(1, 2, '=%s2-%s2-%s2' %
                              (chr(3 + 65), chr(0 + 65), chr(1 + 65)), summary_num_fmt)
-    summary_ws.write_number(1, 3, len(case_lst), summary_num_fmt)
+    summary_ws.write_formula(1, 3, "=_xlfn.COUNTA('%s'!%s:%s) - 1" %
+                             (sheet_name, res_col, res_col), summary_num_fmt)
     summary_ws.write_formula(1, 4, '=%s2/%s2' %
                              (chr(0 + 65), chr(3 + 65)), percent_fmt)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate test report.')
+    parser = argparse.ArgumentParser(description='Generate excel test report.')
     parser.add_argument(
         '-p',
         '--test_plan',
@@ -177,7 +210,6 @@ if __name__ == '__main__':
     logger.info(os.listdir(args.results_dir))
     if len(os.listdir(args.results_dir)) < 2:
         logging.error('No test result in directory %s' % args.results_dir)
-        sys.exit(-1)
 
     report_fn = os.path.abspath(args.report)
     if not os.path.exists(os.path.dirname(report_fn)):
