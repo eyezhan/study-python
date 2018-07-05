91a94e53dfb71d5f161b18ea264c7b59188fd9cd
Zhan Lingling <zhanlingling_00@163.com>
2018-07-05 11:37:38 +0800

    Add set up script for Windows.

---
+++ ai-auto-test/setup_env.bat
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install -y git
+choco install -y python2
+pip install requests
+pip install xlsxwriter
